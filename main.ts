736f48c776b9be67e6cdb2e3868685b79997f826
clon10-w <150766143+clon10-w@users.noreply.github.com>
2024-01-15 14:06:41 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 //% color=026 weight=100 icon="\uf26c" block="CocoCloud Hacker"
 namespace CocoCloudHacker {
 
-	let cocoSent = false
+let cocoSent = false
 	
     //% subcategory="Future Fx"
     //% weight=29
     //% blockGap=8
     //% blockId=send_cococloud
-    //% block="send message to CocoCloud:|API Key %apiKeysec|Properties %pps|Value %vle"
+    //% block="send to CocoCloud:|API Key %apiKeysec|Properties %pps|Value %vlen"
     export function sendToCococloud(apiKeysec: string, pps: string, vlen: number) {
 
         // Reset the upload successful flag.
@@ -39,6 +39,53 @@ namespace CocoCloudHacker {
             esp8266.sendCommand("AT+CIPCLOSE", "OK", 1000)
             return
         }*/
+
+        // Close the connection.
+        esp8266.sendCommand("AT+CIPCLOSE", "OK", 1000)
+
+        // Set the upload successful flag and return.
+        cocoSent = true
+        return
+    }
+
+    //% subcategory="Future Fx"
+    //% weight=29
+    //% blockGap=8
+    //% blockId=send_cococloud_x3
+    //% block="send greenhouse data to CocoCloud:|API Key %apiKeyth|Properties1 %pps1 Value1 %vlen1|Properties2 %pps2 Value2 %vlen2|Properties3 %pps3 Value3 %vlen3"
+    export function sendToCococloudx3(apiKeyth: string, pps1: string, vlen1: number, pps2: string, vlen2: number, pps3: string, vlen3: number) {
+
+        // Reset the upload successful flag.
+        cocoSent = false
+	var vle1 = vlen1.toString()
+	var vle2 = vlen2.toString()
+	var vle3 = vlen3.toString()
+
+        // Make sure the WiFi is connected.
+        if (esp8266.isWifiConnected() == false) return
+
+        // Connect to Telegram. Return if failed is deleted
+        esp8266.sendCommand("AT+CIPSTART=\"TCP\",\"" + "api.cocorobo.hk" + "\",80", "OK", 60)
+
+        // Construct the data to send.
+	let data = "POST /iot/data/eventAPIKeyJson/" + apiKeyth + "/ HTTP/1.1\r\n"
+	data += "Host: " + "api.cocorobo.hk" + "\r\n"
+	data += "Content-Type: " + "application/json" + "\r\n"
+	//data += "Content-Type: " + "text/plain" + "\r\n"
+	data += "Content-Length: " + (pps.length + vle.length + 19) + "\r\n" 
+	data += "\r\n"
+	data += "\{\"" + pps1 + "\":\"" + vle1+ "\",\"" + pps2 + "\":\"" + vle2+ "\",\"" + pps3 + "\":\"" + vle3+ "\"}" 
+
+        // Send the data.
+        esp8266.sendCommand("AT+CIPSEND=" + (data.length + 2))
+        esp8266.sendCommand(data)
+
+        //Return if "SEND OK" is not received.
+        if (esp8266.getResponse("EVENT UPDATE", 1000) == "") {
+            // Close the connection and return.
+            esp8266.sendCommand("AT+CIPCLOSE", "OK", 1000)
+            return
+        }
 
         // Close the connection.
         esp8266.sendCommand("AT+CIPCLOSE", "OK", 1000)
